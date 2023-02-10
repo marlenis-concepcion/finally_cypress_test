70648b429b37895a26de09630a3339842beb896e
marlenis-concepcion <marlenis.concepci@gmail.com>
2023-02-10 08:24:23 -0400

    reports added

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,28 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  video: false,
+  env:{
+    baseUrl: 'https://reqres.in'
+
+  },reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    reportTitle: 'finally Testing Report',
+    reportPageTitle: 'Test Automation Results',
+    reportFilename: 'Report',
+    embeddedScreenshots: true,
+    charts: true,
+    inline: true,
+    overwrite: false,
+    autoOpen: false,
+    showPending: false,
+    showSkipped: true,
+    timestamp: "longDate",
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
